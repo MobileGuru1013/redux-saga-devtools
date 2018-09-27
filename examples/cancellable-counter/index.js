e13361f254583e9911159f8c0db7d9c332843d01
Mobile Guru <dragon@dragons-iMac-2.local>
2018-09-27 06:20:47 -0600

    redux-saga-devtools
    A Visual redux-saga monitor

---
+++ examples/cancellable-counter/index.js
@@ -0,0 +1,34 @@
+import "babel-polyfill"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { take } from 'redux-saga/effects'
+import { DockableSagaView, createSagaMonitor } from '../../src'
+
+import reducer from './reducers'
+import rootSaga from './sagas'
+import Counter from './components/Counter'
+
+const monitor = createSagaMonitor()
+const sagaMiddleware = createSagaMiddleware({sagaMonitor: monitor})
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(function* anotherSaga() {
+  yield taake('SOMETHING') // THIS IS INTENTIONAL. Simulate an error on the devtools
+})
+
+
+ReactDOM.render(
+  <div>
+    <Provider store={store}>
+      <div style={{margin: 10}}><Counter /></div>
+    </Provider>
+    <DockableSagaView monitor={monitor}  />
+  </div>,
+  document.getElementById('root')
+);
